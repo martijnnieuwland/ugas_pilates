e877b542c2b94263c5807d7fe57233ad8671391a
Martijn Nieuwland <martijn@nieuwland.mn>
2024-07-14 00:39:18 +0200

    Content layout and navbar sticky

--- ugas_pilates/static/ugas_pilates/javascript/javascript.js
+++ ugas_pilates/static/ugas_pilates/javascript/javascript.js
@@ -1,7 +1,8 @@
 const navbar = document.querySelector('.navbar-custom');
 
 window.addEventListener('scroll', function () {
-  if (window.scrollY > 50) { // Change 50 to the scroll distance you want
+  if (window.scrollY > 10) {
+    // Change 50 to the scroll distance you want
     navbar.classList.add('scrolled');
   } else {
     navbar.classList.remove('scrolled');
