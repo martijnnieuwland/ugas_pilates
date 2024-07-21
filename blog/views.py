f420b842bba0986ce9777a587a4efe7063043829
Martijn Nieuwland <martijn@nieuwland.mn>
2024-07-21 14:20:22 +0200

    Individual page titles for seo

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,17 @@ from .models import Post
 
 def index(request):
     posts = Post.objects.all()
-    context = {"posts": posts}
+    context = {"posts": posts,
+               "title": "Uga's Blog",
+                "heading": "True to the Core!",
+                "seo": "All about True Pilates in Antwerp, Belgium",}
     return render(request, "blog/index.html", context=context)
 
 
 def detail(request, pk):
     post = Post.objects.get(pk=pk)
-    context = {"post": post}
+    context = {"post": post,
+               "title": "Uga's Blog",
+                "heading": post.title,
+                "seo": "All about True Pilates in Brasschaat, Belgium",}
     return render(request, "blog/detail.html", context)
--- ugas_pilates/views.py
+++ ugas_pilates/views.py
@@ -3,41 +3,91 @@ import os
 from django.shortcuts import render
 
 
-def blog(request):
-    return render(request, 'ugas_pilates/blog.html')
-
 def contact(request):
     context = {
+        "title": "Contact",
+        "heading": "Get in Touch",
+        "seo": "10 minutes from Antwerp",
         "GM_KEY": os.getenv("GM_KEY"),
     }
     return render(request, 'ugas_pilates/contact.html', context)
 
 def faq(request):
-    return render(request, 'ugas_pilates/faq.html')
+    context = {
+        "title": "FAQ",
+        "heading": "Frequently Asked Questions",
+        "seo": "10 commom questions about True Pilates",
+    }
+    return render(request, 'ugas_pilates/faq.html', context)
 
 def home(request):
-    return render(request, 'ugas_pilates/index.html')
+    context = {
+        "title": "Uga's Pilates - True Pilates",
+        "heading": "Getting to your core",
+    }
+    return render(request, 'ugas_pilates/index.html', context)
 
 def instructors(request):
-    return render(request, 'ugas_pilates/instructors.html')
+    context = {
+        "title": "Instructors",
+        "heading": "Meet Your Instructor",
+        "seo": "Dedicated to your wellbeing",
+    }
+    return render(request, 'ugas_pilates/instructors.html', context)
 
 def pricing(request):
-    return render(request, 'ugas_pilates/pricing.html')
+    context = {
+        "title": "Pricing",
+        "heading": "Your Value",
+        "seo": "Train for only €20",
+    }
+    return render(request, 'ugas_pilates/pricing.html', context)
 
 def privacy(request):
-    return render(request, 'ugas_pilates/privacy-policy.html')
+    context = {
+        "title": "Privacy Policy",
+        "heading": "Privacy Policy",
+    }
+    return render(request, 'ugas_pilates/privacy-policy.html', context)
 
 def schedule(request):
-    return render(request, 'ugas_pilates/schedule.html')
+    context = {
+        "title": "Schedule",
+        "heading": "Studio Opening Hours",
+        "seo": "Flexible hours to accomodate your schedule",
+    }
+    return render(request, 'ugas_pilates/schedule.html', context)
 
 def studio(request):
-    return render(request, 'ugas_pilates/studio.html')
+    context = {
+        "title": "Uga's Pilates",
+        "heading": "The Studio",
+        "seo": "Following Romana Kryzanowska's true pilates method",
+    }
+    return render(request, 'ugas_pilates/studio.html', context)
 
 def terms(request):
-    return render(request, 'ugas_pilates/terms-of-service.html')
+    context = {
+        "title": "Terms of Service",
+        "heading": "Terms of Service",
+    }
+    return render(request, 'ugas_pilates/terms-of-service.html', context)
 
 def testimonials(request):
-    return render(request, 'ugas_pilates/testimonials.html')
+    context = {
+        "title": "Testimonials",
+        "heading": "Hear it from our hard core!",
+        "seo": "Why our customers choose us",
+    }
+    return render(request, 'ugas_pilates/testimonials.html', context)
 
+
+
+# Not included in urls.  Only for use as playground in project environment -->
 def testbed(request):
-    return render(request, 'ugas_pilates/testbed.html')
+    context = {
+        "title": "Testimonials",
+        "heading": "Hear it from our hard core!",
+        "seo": "Why our customers choose us",
+    }
+    return render(request, 'ugas_pilates/testbed.html', context)
